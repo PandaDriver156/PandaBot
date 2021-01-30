42cae6956af6023da5695fa6400618c1988e6beb
PandaDriver <pandadriver156@gmail.com>
2021-01-30 18:48:43 +0100

    Add more fake-offline detection

--- src/client.ts
+++ src/client.ts
@@ -2,9 +2,9 @@ import * as BetterSqlite3 from "better-sqlite3";
 import * as Discord from "discord.js";
 import * as path from "path";
 import { Commander } from "./commander";
-import { Colors, Config, UserTrack } from "./types";
+import { Colors, Config, FakeOfflineInfo, Message, UserTrack } from "./types";
 import { VoiceManager } from "./voice";
-import { getPresenceData } from "./utility"
+import { getPresenceData, wait } from "./utility"
 
 export interface ClientOptions extends Discord.ClientOptions
 {
@@ -29,25 +29,57 @@ export class Client extends Discord.Client
             client: this,
             prefix: this.config.prefix,
             path: path.join(__dirname, 'commands'),
-            allowBots: false
+            allowBots: false,
+            autoHandleMessages: false
         });
         if (this.config.track_voice)
             this.voiceManager = new VoiceManager(this);
 
-        this.db.prepare("CREATE TABLE IF NOT EXISTS tracked_users (channel TEXT, tracked_user TEXT PRIMARY KEY)").run();
+        this.db.prepare("CREATE TABLE IF NOT EXISTS tracked_users (guild TEXT, channel TEXT, tracked_user TEXT)").run();
         this.db.prepare("CREATE TABLE IF NOT EXISTS user_presences (id TEXT, date TEXT, data TEXT)").run();
-        this.tracked_users = this.db.prepare("SELECT channel, tracked_user FROM tracked_users").all().map(result =>
+        this.tracked_users = this.db.prepare("SELECT * FROM tracked_users").all().map(result =>
         {
             return {
-                user_id: result.tracked_user,
-                channel_id: result.channel,
-                last_presence: null
+                guild: result.guild,
+                user: result.tracked_user,
+                channel: result.channel,
+                last_presence: null,
+                last_fake_offline_at: null
             };
         });
 
+        this.on("message", this.onMessage)
         this.on("ready", this.onReady);
-        this.on("presenceUpdate", this.onPresenceUpdate);
+        if (!this.config.presence)
+            this.on("presenceUpdate", this.onPresenceUpdate);
         this.on("typingStart", this.onTypingStart);
+        this.on("voiceStateUpdate", this.onVoiceStateChange)
+    }
+
+    async onMessage(message: Message)
+    {
+        this.commander.handleMessage(message);
+
+        if (message.author.bot) return;
+
+        const trackInfos = this.tracked_users.filter(u => u.user === message.author.id)
+        if (!trackInfos.length) return;
+        if (!["online", "dnd"].includes(message.author.presence.status))
+        {
+            for (const trackInfo of trackInfos)
+            {
+                await this.logFakeOffline({
+                    channel: trackInfo.channel,
+                    user: message.author,
+                    action: ":inbox_tray: Sent a message",
+                    extraInfo: [
+                        `Server: ${message.guild.name}`,
+                        `Channel: ${message.channel}`
+                    ]
+                });
+                await wait(500);
+            }
+        }
     }
 
     onPresenceUpdate(oldPresence: Discord.Presence, newPresence: Discord.Presence)
@@ -61,36 +93,106 @@ export class Client extends Discord.Client
         console.log("Ready!");
 
         this.mainUser = this.users.cache.get(this.config.permLevels.BotOwner) || await this.users.fetch(this.config.permLevels.BotOwner);
-        this.updatePresence(true);
+
+        if (this.config.presence)
+            this.user.setPresence({
+                status: this.config.presence
+            });
+        else this.updatePresence(true);
 
         setInterval(this.checkPresences.bind(this), this.config.presence_check_interval);
     }
 
-    onTypingStart(channel: Discord.Channel, user: Discord.User)
+    async onTypingStart(channel: Discord.GuildChannel, user: Discord.User)
     {
-        const trackInfo = this.tracked_users.find(u => u.user_id === user.id);
-        if (!trackInfo) return;
-        if (user.presence.status !== "online" && user.presence.status !== "dnd") this.logFakeOffline(trackInfo.channel_id, trackInfo.user_id, "typing in a channel");
+        if (user.bot) return;
+        const trackInfos = this.tracked_users.filter(u => u.user === user.id);
+        if (!trackInfos.length) return;
+        if (user.presence.status !== "online" && user.presence.status !== "dnd")
+            for (const trackInfo of trackInfos)
+            {
+                await this.logFakeOffline({
+                    channel: trackInfo.channel,
+                    user: user,
+                    action: ":keyboard: Started typing in a channel",
+                    extraInfo: [
+                        `Server: ${channel.guild.name}`,
+                        `Channel: ${channel.name} (${channel})`
+                    ]
+                });
+                await wait(500);
+            }
+
     }
 
-    logFakeOffline(channelResolv: Discord.ChannelResolvable, userResolv: Discord.UserResolvable, action: string)
+    async onVoiceStateChange(oldState: Discord.VoiceState, newState: Discord.VoiceState)
     {
-        const channel = this.channels.resolve(channelResolv) as Discord.TextChannel;
-        const user = this.users.resolve(userResolv);
+        if (newState.member.user.bot) return;
+        const trackInfos = this.tracked_users.filter(u => u.user === newState.member.id);
+        if (!trackInfos.length) return;
+        if (!["online", "dnd"].includes(newState.member.user.presence.status))
+        {
+            if (newState.channelID && oldState.channelID !== newState.channelID)
+                for (const trackInfo of trackInfos)
+                {
+                    await this.logFakeOffline({
+                        channel: trackInfo.channel,
+                        user: newState.member.user,
+                        action: ":speaker: Joined a voice channel",
+                        extraInfo: [
+                            `Server: ${newState.guild.name}`,
+                            `Channel: ${newState.channel.name}`
+                        ]
+                    });
+                    await wait(500);
+                }
+
+            else if (!oldState.streaming && newState.streaming)
+            {
+                for (const trackInfo of trackInfos)
+                {
+                    await this.logFakeOffline({
+                        channel: trackInfo.channel,
+                        user: newState.member,
+                        action: ":desktop: Started streaming in a voice channel",
+                        extraInfo: [
+                            `Server: ${newState.guild.name}`,
+                            `Channel: ${newState.channel.name}`
+                        ]
+                    });
+                    await wait(500);
+                }
+            }
+        }
+    }
+
+    logFakeOffline(info: FakeOfflineInfo)
+    {
+        const channel = this.channels.resolve(info.channel) as Discord.TextChannel;
+        const user = this.users.resolve(info.user);
+        const trackInfo = this.tracked_users.find(u => u.user === user.id && u.channel === info.channel);
+        if ((Date.now() - trackInfo.last_fake_offline_at?.getTime()) < 300000) return;
+        const lines = [
+            `User: ${user.tag}`,
+            `Action: ${info.action}`,
+            `Status: ${this.config.emojis[user.presence.status as "offline" | "online" | "idle"]} ${user.presence.status}`
+        ];
+        if (info.extraInfo) lines.push(...info.extraInfo);
+        trackInfo.last_fake_offline_at = new Date();
         const embed = new Discord.MessageEmbed()
             .setColor(Colors.red)
             .setTitle(":warning: Bad boi detected")
-            .setThumbnail(user.avatarURL())
-            .setDescription(`User: ${user.tag} \nAction: ${action} \nStatus: ${this.config.emojis[user.presence.status as "offline" | "online" | "idle"]} ${user.presence.status}`)
+            .setThumbnail(user.avatarURL({ format: "png", size: 256 }))
+            .setDescription(lines.join("\n"))
             .setTimestamp();
-            channel.send(embed);
+        return channel.send(embed);
     }
 
     checkPresences(users = this.tracked_users)
     {
         for (const userTrack of users)
         {
-            const user = this.users.cache.get(userTrack.user_id);
+            const user = this.users.cache.get(userTrack.user);
             const presenceData = getPresenceData(user.presence);
             if (userTrack.last_presence?.status !== presenceData.status ||
                 JSON.stringify(userTrack.last_presence?.devices) !== JSON.stringify(presenceData.devices))
--- src/commands/track.ts
+++ src/commands/track.ts
@@ -1,49 +1,129 @@
-import { AccessLevels, Message, BaseCommand } from "../types";
+import { GuildChannel, MessageEmbed } from "discord.js";
+import { AccessLevels, BaseCommand, Colors, Message } from "../types";
 import { getPresenceData } from "../utility";
 
-export default class TrackCommand implements BaseCommand
-{
-    async execute(message: Message, args: string[])
+const command: BaseCommand = {
+    async execute(message, args)
     {
-        const user = message.client.users.cache.get(message.mentions.members.first()?.id) ||
-            message.client.users.cache.get(args[0]) ||
-            message.client.users.cache.find(u => u.tag === args[0]);
-
-        const channel = message.mentions.channels.first() || message.client.channels.cache.get(args[1]);
-
-        if (!user)
+        switch (args[0])
         {
-            message.channel.send(":x: User not found");
-            return;
+            case "add":
+                addUser(message, args.slice(1));
+                break;
+            case "remove":
+                removeUser(message, args.slice(1));
+                break;
+            case "list":
+                const trackedUsers = message.client.tracked_users
+                    .filter(u => u.guild === message.guild.id)
+                    .map(u =>
+                    {
+                        const user = message.client.users.cache.get(u.user);
+                        return `${user.tag} (${user.toString()})`;
+                    });
+
+                if (!trackedUsers.length)
+                {
+                    const embed = new MessageEmbed()
+                        .setColor(message.guild.me.displayColor || Colors.lightBlue)
+                        .setDescription("No users are tracked on this server");
+
+                    message.channel.send(embed);
+                    return;
+                }
+
+                const embed = new MessageEmbed()
+                    .setTitle("Tracked Users")
+                    .setColor(message.guild.me.displayColor || Colors.lightBlue)
+                    .setDescription(trackedUsers.join("\n"))
+                    .setTimestamp();
+
+                message.channel.send(embed);
+                break;
+            default:
+                message.channel.send(":x: Invalid subcommand provided. Valid ones are: add, remove, list")
+                break;
         }
-
-        if (!channel)
-        {
-            message.channel.send(":x: Please specifiy channel where presence updates should be logged");
-            return;
-        }
-
-        // const exists = message.client.db.prepare("SELECT * from tracked_users WHERE traced_user_id = ?").get(user.id);
-        const exists = message.client.tracked_users.some(u => u.user_id === user.id);
-
-        if (exists)
-        {
-            message.channel.send(":x: This user is already tracked");
-            return;
-        }
-
-        message.client.db.prepare("INSERT INTO tracked_users (channel, tracked_user) VALUES (?, ?)").run(channel.id, user.id);
-
-        message.client.tracked_users.push({
-            user_id: user.id,
-            channel_id: channel.id,
-            last_presence: getPresenceData(user.presence)
-        });
-
-        message.channel.send(`:white_check_mark: \`${user.tag}\`'s presence changes are now saved`);
-    }
-
-    info = {
+    },
+    info: {
         level: AccessLevels.BotAdmin
     }
-}
+};
+
+const addUser = (message: Message, args: string[]) =>
+{
+    const user = message.client.users.cache.get(message.mentions.members.first()?.id) ||
+        message.client.users.cache.get(args[0]) ||
+        message.client.users.cache.find(u => u.tag === args[0]);
+
+    const channel = (message.mentions.channels.first() || message.client.channels.cache.get(args[1])) as GuildChannel;
+
+    if (!user)
+    {
+        message.channel.send(":x: User not found");
+        return false;
+    }
+
+    if (!channel)
+    {
+        message.channel.send(":x: Please specifiy channel where presence updates should be logged");
+        return false;
+    }
+
+    if (user.id === message.client.user.id)
+    {
+        message.channel.send("Don't worry i'll never be fake-offline :wink:");
+        return false;
+    }
+
+    // const exists = message.client.db.prepare("SELECT * from tracked_users WHERE traced_user_id = ?").get(user.id);
+    const exists = message.client.tracked_users.some(u => u.guild === message.guild.id && u.user === user.id);
+
+    if (exists)
+    {
+        message.channel.send(":x: This user is already tracked");
+        return false;
+    }
+
+    message.client.db.prepare("INSERT INTO tracked_users (guild, channel, tracked_user) VALUES (?, ?, ?)").run(channel.guild.id, channel.id, user.id);
+
+    message.client.tracked_users.push({
+        guild: channel.guild.id,
+        user: user.id,
+        channel: channel.id,
+        last_presence: getPresenceData(user.presence),
+        last_fake_offline_at: null
+    });
+
+    message.channel.send(`:white_check_mark: \`${user.tag}\`'s presence changes are now saved`);
+    return true;
+};
+
+const removeUser = (message: Message, args: string[]) =>
+{
+    const user = message.client.users.cache.get(message.mentions.members.first()?.id) ||
+        message.client.users.cache.get(args[0]) ||
+        message.client.users.cache.find(u => u.tag === args[0]);
+
+    const trackInfoIndex = message.client.tracked_users.findIndex(t => t.guild === message.guild.id && t.user === user.id);
+    const trackInfo = message.client.tracked_users[trackInfoIndex];
+
+    if (!user || !trackInfo)
+    {
+        const embed = new MessageEmbed()
+            .setColor(Colors.red)
+            .setDescription("User not found or is not tracked");
+        message.channel.send(embed);
+        return;
+    }
+
+    message.client.db.prepare("DELETE FROM tracked_users WHERE guild = ? AND tracked_user = ?").run(trackInfo.guild, trackInfo.user);
+    message.client.tracked_users.splice(trackInfoIndex, 1);
+
+    const embed = new MessageEmbed()
+        .setColor(Colors.green)
+        .setDescription(`${user.tag} is no longer tracked`);
+    message.channel.send(embed);
+};
+
+export default command;
